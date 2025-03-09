719573391bcd345a48a2eec4e24f4b1cb5f3e4ff
Vuong Nguyen <yeungon@gmail.com>
2025-03-09 17:40:27 +0700

    Add test

--- vphong_test.go
+++ vphong_test.go
@@ -11,4 +11,13 @@ func TestRunWithInput(t *testing.T) {
 	} else {
 		t.Logf("wanted: %s, got: %s", wanted, result)
 	}
+	input2 := "đầu lòng hai ả Tố Nga"
+	result2 := RunWithInput(input2, "n", false, false, false, false, false, "", "", false)
+	wanted2 := "ɗəwᴮ¹ lɔŋ͡mᴬ¹ haːjᴬ¹ ʔaːᴮ¹ toːᴮ¹ ŋaːᴬ¹"
+	if result2 != wanted2 {
+		t.Errorf("wanted: %s, got: %s", wanted, result)
+	} else {
+		t.Logf("wanted: %s, got: %s", wanted, result)
+	}
+
 }
